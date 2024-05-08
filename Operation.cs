cad43013c5a636b07c65da1c8f7433daa88ee570
Your Name <you@example.com>
2024-05-08 22:58:03 +0500

    Add Calculate to operation

--- Operation.cs
+++ Operation.cs
@@ -10,6 +10,27 @@ class Operation : Token
     public char Op { get; }
     public int Precedence { get; set; }
 
+    public float Calculate(float left, float right)
+    {
+        if (Op == '+')
+        {
+            return left + right;
+        }
+        if (Op == '-')
+        {
+            return left - right;
+        }
+        if (Op == '*')
+        {
+            return left * right;
+        }
+        if (Op == '/')
+        {
+            return left / right;
+        }
+        return 0;
+    }
+
     public override string ToString()
     {
         return Op.ToString();
