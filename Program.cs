33681d6155063597e854dcfcf64d303084e78822
Your Name <you@example.com>
2024-05-08 12:21:22 +0500

    Lab 1 ready

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Tuple<List<int>, List<char>> ParseInput(string input)
+{
+    var numbers = new List<int>();
+    var operators = new List<char>();
+    var number = 0;
+    foreach (var c in input)
+    {
+        if (c == ' ')
+        {
+            if (number != -1)
+            {
+                numbers.Add(number);
+                number = -1;
+            }
+        }
+        else if (c == '+' || c == '-' || c == '*' || c == '/')
+        {
+            if (number != -1){
+                numbers.Add(number);
+                number = -1;
+            }
+            operators.Add(c);
+        }
+        else
+        {
+            if (number == -1)
+            {
+                number = 0;
+            }
+            number = number * 10 + (c - '0');
+        }
+    }
+    numbers.Add(number);
+
+    return new Tuple<List<int>, List<char>>(numbers, operators);
+}
+
+Console.WriteLine("Enter an expression: ");
+
+string? input = Console.ReadLine();
+
+var (numbers, operators) = ParseInput(input ?? "");
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    Console.Write(numbers[i] + " ");
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < operators.Count; i++)
+{
+    Console.Write(operators[i] + " ");
+}
