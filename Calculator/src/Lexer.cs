0b6fae86ac97ccf1bae8b08c302336643d7525e5
Your Name <you@example.com>
2024-06-04 13:31:16 +0500

    Add comma separated function arguments support

--- Calculator/src/Lexer.cs
+++ Calculator/src/Lexer.cs
@@ -47,7 +47,7 @@ public class Lexer
 
     private void SkipWhitespace()
     {
-        while (_position < _input.Length && char.IsWhiteSpace(_input[_position]))
+        while (_position < _input.Length && (char.IsWhiteSpace(_input[_position]) || _input[_position] == ','))
         {
             _position++;
         }
